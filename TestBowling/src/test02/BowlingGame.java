4c0d465d73cbf65c4736d00458cd3bc473301846
martin <martin@Gladice>
2021-03-21 22:11:09 +0100

    neue Version Sortierung.

--- TestBowling/src/test02/BowlingGame.java
+++ TestBowling/src/test02/BowlingGame.java
@@ -32,18 +32,35 @@ class Bowling {
 	}
 
 	// Version 2 mit Stream
-	public void getWinner() {
+	public void getWinnerV2() {
 		// Ausgabe aller Spieler:
 		System.out.println(players);
 
 		// Comparator um die Entrys vergleichbar zu machen.
 
-		Optional<Entry<String, Integer>> winner = 
-				players.entrySet().stream().sorted( (entry1, entry2) -> entry2.getValue() - entry1.getValue()).findFirst() ;
+		Optional<Entry<String, Integer>> winner = players.entrySet().stream()
+				.sorted((entry1, entry2) -> entry2.getValue() - entry1.getValue()).findFirst();
 		if (winner.isPresent()) {
 			System.out.println("Der Sieger ist:" + winner.get().getKey() + " Punkte:" + winner.get().getValue());
 		}
 
+	}
+
+	// Version 3 mit einem Durchlauf über die Entrys der HashMap um den Entry mit dem größten Value zu finden.
+	public void getWinner() {
+		// Ausgabe aller Spieler:
+		System.out.println(players);
+		Entry<String, Integer> theWinner = null;
+		for (Entry<String, Integer> actEntry : players.entrySet()) {
+			if (theWinner == null) {
+				theWinner = actEntry;
+			} else if (actEntry.getValue() > theWinner.getValue()) {
+				theWinner = actEntry;
+			}
+		}
+		if (theWinner != null) {
+			System.out.println("Der Sieger ist:" + theWinner.getKey() + " Punkte:" + theWinner.getValue());
+		}
 	}
 }
 
